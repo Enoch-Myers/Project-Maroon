75a661808a641278435b06024b3b47bcd9064cb4
porkchopmiles <mileshandlen@gmail.com>
2025-02-24 15:46:19 -0800

    resubmit

--- UnityGame/Peghead/Assets/Scenes/Levels/Tutorial assets/Finish_Line.cs
+++ UnityGame/Peghead/Assets/Scenes/Levels/Tutorial assets/Finish_Line.cs
@@ -19,7 +19,7 @@ public class Finish_Line : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Tutorial Boss");
+            SceneManager.LoadScene("Tutorial_Boss");
         }
     }
 }
