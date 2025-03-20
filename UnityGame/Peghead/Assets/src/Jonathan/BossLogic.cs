81a26c5af07c237523b1495b1848befa80e33d12
JonPal7756 <palm7756@vandals.uidaho.edu>
2025-03-20 00:53:16 -0700

    tests
    after merging main into my branch

--- UnityGame/Peghead/Assets/src/Jonathan/BossLogic.cs
+++ UnityGame/Peghead/Assets/src/Jonathan/BossLogic.cs
@@ -5,14 +5,32 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public Transform player;
     public float speed = 2f;
 
-    void Update()
-    {
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public int CurrentHealth{
+        get { return currentHealth; }
+    }
+
+    void Start(){
+        currentHealth = maxHealth;
+    }
+
+    void Update(){
+        // Basic "follow/avoid" logic if player is assigned
         if(player != null){
-            if(transform.position.x < player.position.x) {
+            if(transform.position.x < player.position.x){
                 transform.position += Vector3.right * speed * Time.deltaTime;
-            } else if(transform.position.x > player.position.x) {
+            }
+            else if(transform.position.x > player.position.x){
                 transform.position += Vector3.left * speed * Time.deltaTime;
             }
         }
+    }
+
+    public void TakeDamage(int damageAmount){
+        if(currentHealth < 0) currentHealth = 0;
+
+        if(currentHealth == 0) Debug.Log("Boss defeated!");
     }
 }
